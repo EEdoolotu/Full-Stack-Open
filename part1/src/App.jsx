29afb5af3d261e202ad3a05b9f2a926bd6cf5889
EEdoolotu <ejirogheneedoolotu@gmail.com>
2026-03-07 17:59:32 +0000

    new

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,9 +1,58 @@
-const App = () => {
+const Header = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>The name of the course is {props.course}</p>
     </div>
   )
 }
 
-export default App
+const Part = (props) => {
+  return (
+    <p>{props.name}{props.exercise}</p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.part1} exercise={props.exercises1}/>
+      <Part name={props.part2} exercise={props.exercises2}/>
+      <Part name={props.part3} exercise={props.exercises3}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>The total number of exercises is {props.exercise}</p>
+    </div>
+  )
+}
+
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content 
+        part1={part1} exercise1={exercises1}
+        part2={part2} exercise2={exercises2}
+        part3={part3} exercise3={exercises3}
+      />
+      
+      <Total exercise={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App 
