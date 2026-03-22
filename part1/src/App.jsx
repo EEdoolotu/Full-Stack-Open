6412b89d755221f9632d71d580ffe6e3555711d7
EEdoolotu <ejirogheneedoolotu@gmail.com>
2026-03-22 14:37:25 +0000

    new

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,58 +1,18 @@
-const Header = (props) => {
-  return (
-    <div>
-      <p>The name of the course is {props.course}</p>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>{props.name}{props.exercise}</p>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part name={props.part1} exercise={props.exercises1}/>
-      <Part name={props.part2} exercise={props.exercises2}/>
-      <Part name={props.part3} exercise={props.exercises3}/>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>The total number of exercises is {props.exercise}</p>
-    </div>
-  )
-}
-
-
+import { useState } from 'react'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+
+  const [ counter, setCounter ] = useState(0)
+
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
 
   return (
-    <div>
-      <Header course={course} />
-      <Content 
-        part1={part1} exercise1={exercises1}
-        part2={part2} exercise2={exercises2}
-        part3={part3} exercise3={exercises3}
-      />
-      
-      <Total exercise={exercises1 + exercises2 + exercises3} />
-    </div>
+    <div>{counter}</div>
   )
 }
 
-export default App 
+export default App
